e8b012d93cf9aa46d2f3fbddeee79da8261e78e8
WellingtonGuedesDev <ofwellington2@gmail.com>
2025-03-17 01:11:24 -0300

    Adicionado carroseis

--- src/assets/components/projectDetail/ProjectDetail.jsx
+++ src/assets/components/projectDetail/ProjectDetail.jsx
@@ -3,7 +3,16 @@ import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from "react-ico
 import { colors } from '../../styles/colors';
 import { ThemeContext } from '../../../ThemeContext';
 import { useContext } from 'react';
+import { useState, useEffect } from 'react';
 
+// Swipper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// import required modules Swiper
+import { FreeMode, Pagination, Autoplay } from 'swiper/modules';
 
 const projects = [
     {
@@ -77,18 +86,31 @@ const projects = [
 const ProjectDetail = () => {
     const { id } = useParams();
     const { theme } = useContext(ThemeContext)
+    const [screenWidth, setScreenWidth] = useState(false);
     const currentColor = colors[theme];
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+          
+          if (window.innerWidth < 1200) {
+            setScreenWidth(true)
+            console.log(screenWidth)
+          } else {
+            setScreenWidth(false)
+          }
+        });
+      }, [screenWidth]);
 
     return (
         <>
             <div className='hidden xl:block object-contain fixed top-[50%] left-[4rem] translate-y-[-50%]'>
                 <Link to={parseInt(id) === 1 ? `/projeto/${projects.length}` : `/projeto/${parseInt(id) - 1}`}>
-                <MdKeyboardDoubleArrowLeft className={`text-5xl text-[${currentColor.primary}] animate-slide-in-from-right cursor-pointer`} />
+                    <MdKeyboardDoubleArrowLeft className={`text-5xl text-[${currentColor.primary}] animate-slide-in-from-right cursor-pointer`} />
                 </Link>
             </div>
             <div className='hidden xl:block object-contain fixed top-[50%] right-[4rem] translate-y-[-50%]'>
                 <Link to={parseInt(id) === projects.length ? `/projeto/1` : `/projeto/${parseInt(id) + 1}`}>
-                <MdKeyboardDoubleArrowRight className={`text-5xl text-[${currentColor.primary}] animate-slide-in-from-left cursor-pointer`} />
+                    <MdKeyboardDoubleArrowRight className={`text-5xl text-[${currentColor.primary}] animate-slide-in-from-left cursor-pointer`} />
                 </Link>
             </div>
 
@@ -105,31 +127,57 @@ const ProjectDetail = () => {
                     </div>
 
                     <div className='flex flex-col xl:flex-row mt-12 gap-8 md:mt-12 md:justify-start md:gap-[10rem] md:mt-[6rem]'>
-                        <div className='md:w-[250px]'>
-                            <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Cliente</h3>
 
-                            <p className='text-base/5'>{projects[id - 1].client}</p>
-                        </div>
-                        <div className='md:w-[250px]'>
-                            <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Funcionalidades</h3>
 
-                            <p className='text-base/5'>{projects[id - 1].features}</p>
-                        </div>
-                        <div className='md:w-[250px]'>  
-                            <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Timeline</h3>
 
-                            <p className='text-base/5'>{projects[id - 1].timeline}</p>
-                        </div>
-                        <div className='md:w-[250px]'>
-                            <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Stack</h3>
 
-                            <p className='text-base/5'>{projects[id - 1].stack.join(', ')}</p>
-                        </div>
+
+                        <Swiper
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            slidesPerView={1}
+                            spaceBetween={30}
+                            freeMode={true}
+                            width={250}
+                            modules={[FreeMode, Pagination, Autoplay]}
+                            className="mySwiper w-full"
+                        >
+                            <SwiperSlide>
+                                <div className='md:w-[250px] flex flex-col items-center'>
+                                    <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Cliente</h3>
+
+                                    <p className='text-base/5'>{projects[id - 1].client}</p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className='md:w-[250px]'>
+                                    <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Funcionalidades</h3>
+
+                                    <p className='text-base/5'>{projects[id - 1].features}</p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className='md:w-[250px]'>
+                                    <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Timeline</h3>
+
+                                    <p className='text-base/5'>{projects[id - 1].timeline}</p>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className='md:w-[250px]'>
+                                    <h3 className={`text-xl text-[${currentColor.textTransparent}] font-semibold mb-4`}>Stack</h3>
+
+                                    <p className='text-base/5'>{projects[id - 1].stack.join(', ')}</p>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
 
-            <div className='my-[10rem]'>
+            <div className='my-[6rem] md:my-[10rem]'>
                 <div className='info max-w-[1200px] mx-auto px-4'>
                     <p className={`font-bold antialiased text-[${currentColor.primary}] text-base mb-2`}>Overview</p>
 
@@ -174,12 +222,26 @@ const ProjectDetail = () => {
                     <h2 className='text-3xl font-bold antialiased mb-4'>Projeto Final</h2>
                     <p className='text-base/5 mb-8'>Eu era responsável pela pesquisa de usuários, prototipagem, design de pôsteres e edição de vídeo. Criei uma renderização 3D de um espaço de descoberta usando Cinema 4D, usei Figma para design de pôsteres e Adobe Premiere para edição de vídeo. Todos participaram durante todo o processo geral de ideação, pesquisa e entrevista com usuários.</p>
 
-                    <div className='flex flex-wrap justify-center gap-8'>
-                        <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_0767085b87cd4514beb9319e53ad5e3b~mv2.png/v1/fill/w_619,h_434,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Screen%20Shot%202021-10-31%20at%2010_05_23%20PM.png" alt="" />
-
-                        <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_ab9a1df9609c4a6d8fb1d5669da76396~mv2.gif" alt="" />
-
-                        <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_c99388621e3947eda80a73f2ff382b78~mv2.png/v1/fill/w_255,h_546,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/quiz%203.png" alt="" />
+                    <div className='gap-8'>
+                        <Swiper
+                            autoplay={screenWidth ? { delay: 2500, disableOnInteraction: false, } : undefined}
+                            slidesPerView={screenWidth ? 1 : 1}
+                            spaceBetween={30}
+                            freeMode={true}
+                            width={200}
+                            modules={[FreeMode, Pagination, Autoplay]}
+                            className="mySwiper w-full"
+                        >
+                            <SwiperSlide>
+                                <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_0767085b87cd4514beb9319e53ad5e3b~mv2.png/v1/fill/w_619,h_434,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Screen%20Shot%202021-10-31%20at%2010_05_23%20PM.png" alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_ab9a1df9609c4a6d8fb1d5669da76396~mv2.gif" alt="" />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img className='w-[250px]' src="https://static.wixstatic.com/media/b0335f_c99388621e3947eda80a73f2ff382b78~mv2.png/v1/fill/w_255,h_546,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/quiz%203.png" alt="" />
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
